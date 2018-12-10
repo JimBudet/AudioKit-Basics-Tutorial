c9ee4f8cc4d3115fbf339c4cd14f0f7457c3fe35
Jim Budet <j4l3b4@gmail.com>
2018-12-10 13:10:47 -0600

    Add AudioKit functionality

--- AudioKit-Basics-Tutorial/TableViewController.swift
+++ AudioKit-Basics-Tutorial/TableViewController.swift
@@ -6,11 +6,35 @@
 //  Copyright © 2018 JimBudet. All rights reserved.
 //
 
+import AudioKit
 import UIKit
 
 class TableViewController: UITableViewController {
 
+    var oscillator1 = AKOscillator()
+    var oscillator2 = AKOscillator()
+    var mixer = AKMixer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        oscillator1.frequency = random(in: 220 ... 880)
+        
+        oscillator2.frequency = random(in: 220 ... 880)
+        
+        mixer = AKMixer(oscillator1, oscillator2)
+        mixer.volume = 0.5
+        
+        AudioKit.output = mixer
+        
+        do {
+            try AudioKit.start()
+        }
+        catch {
+            AKLog("AudioKit did not start!")
+        }
+        
+        oscillator1.start()
+        oscillator2.start()
     }
 }
