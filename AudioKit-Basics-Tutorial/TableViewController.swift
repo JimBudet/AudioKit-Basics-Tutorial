259a997a2a78a99fc533e9aac77951aa9e47cbd0
Jim Budet <j4l3b4@gmail.com>
2018-12-10 12:48:27 -0600

    Changes the initial view controller.

---
+++ AudioKit-Basics-Tutorial/TableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewController.swift
+//  AudioKit-Basics-Tutorial
+//
+//  Created by Jaime Budet on 12/10/18.
+//  Copyright © 2018 JimBudet. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
